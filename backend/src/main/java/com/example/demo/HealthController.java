1aaafa640d8c368cf93aaea508aa6e6d64fcfbbe
miinspp <qkralstn8332@hanyang.ac.kr>
2026-07-11 21:47:37 +0900

    feat: health controller 추가

---
+++ backend/src/main/java/com/example/demo/HealthController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    @GetMapping
+    public ResponseEntity<String> healthcheck() {
+        return ResponseEntity.ok().body("ok");
+    }
+}
